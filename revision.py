5e4e650e2c461685af3e4bf4e25dccca21f75f35
Eva-Rose Dolan!! <21edolan.acc@lwetb.ie>
2025-04-28 12:58:18 +0100

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,37 @@
+#071
+sport1='football'
+sport2='hurling'
+lstt=[sport1,sport2]
+print(lstt)
+sport3=input('what is your favourite sport?:')
+sport3.lower()
+lst2=[sport1,sport2,sport3]
+lst2.sort()
+print(lst2)
+
+#072
+sub1='maths'
+sub2='english'
+sub3='irish'
+sub4='biology'
+sub5='spanish'
+sub6='geography'
+subjects=[sub1,sub2,sub3,sub4,sub5,sub6]
+print(subjects)
+dislike=input('which of these subjects do you not like?:')
+subjects.remove(dislike)
+print(subjects)
+
+#074
+colours=['red','orange','yellow','green','blue','indigo','violet','pink','white','black']
+num=int(input('input a starting number between 0-4:'))
+num2=int(input('input an end number between 5-9:'))
+colours[num:num2+1]
+print(colours)
+
+#075
+nums=[111,222,333,444]
+print(nums)
+
+#076
+
